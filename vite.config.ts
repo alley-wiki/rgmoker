f3c27da9181768eb546373f943d562e9c5fc310a
rimuruyeban <eventskritniyagent@gmail.com>
2025-02-20 01:35:56 +0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rgmoker/', // если сайт будет доступен по https://alley-wiki.github.io/rgmoker
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
